6eec0822e5cf1aa40b1e89dbb42cde73aef15f59
flashcy <140628455+flashcy@users.noreply.github.com>
2023-09-06 00:48:37 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2204 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -4,14 +4,13 @@
 using namespace std;
 
 int maze[102][102];
-int visited[102][102];
-int dp[102][102];
+bool visited[102][102];
 int N, M;
 
-queue<pair<int, int>> q;
+queue<pair<int, pair<int, int>>> q;
 int dx[4] = { -1, 0, 1, 0 };
 int dy[4] = { 0, -1, 0, 1 };
-int f_ans = 1000;
+int f_ans = 1000000;
 
 int main() {
 	ios_base::sync_with_stdio(false);
@@ -23,26 +22,31 @@ int main() {
 		for (int j = 1; j <= M; j++) {
 			char k;
 			cin >> k;
-			maze[i][j] = (int)k - 48;
+			maze[i][j] = k - '0';
 		}
 	}
 
-	q.push({ 1,1 });
-	visited[1][1] = 1;
-	dp[1][1] = 1;
-	int x = 0, y = 0;
-	while (!(q.empty() || (x == M && y == N))) {
-		x = q.front().first;
-		y = q.front().second;
+	q.push({ 1,{ 1,1 } });
+	visited[1][1] = true;
+	while (!q.empty()) {
+		int cnt = q.front().first;
+		int y = q.front().second.first;
+		int x = q.front().second.second;
+		q.pop();
+
+		if (x == M && y == N) {
+			f_ans = min(f_ans, cnt);
+			continue;
+		}
 
 		for (int i = 0; i < 4; i++) {
-			if (maze[y + dy[i]][x + dx[i]] == 1 && visited[y + dy[i]][x + dx[i]] == 0) {
-				q.push({ x + dx[i],y + dy[i] });
-				dp[y + dy[i]][x + dx[i]] = dp[y][x] + 1;
-				visited[y + dy[i]][x + dx[i]] = 1;
-			}
+			int ny = y + dy[i];
+			int nx = x + dx[i];
+			if (ny > N || nx > M || ny <= 0 || nx <= 0)continue;
+			if (maze[ny][nx] == 0 || visited[ny][nx]) continue;
+			visited[ny][nx] = true;
+			q.push({ cnt + 1, {ny, nx} });
 		}
-		q.pop();
 	}
-	cout << dp[N][M] << '\n';
+	cout << f_ans << '\n';
 }
